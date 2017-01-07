7a2b82dd48033424fc5b5beaa5c1e5128f69f559
Suraj Singh Bisht <surajsinghbisht054@gmail.com>
2017-01-08 00:34:29 +0530

    Latest Update

--- whois/whois.py
+++ whois/whois.py
@@ -18,24 +18,41 @@ __author__='''
     Note: We Feel Proud To Be Indian
 ######################################################
 '''
-# Importing Module
+from bs4 import BeautifulSoup
 import urllib2, sys
-# =============================================
-# 		Configurations
-#
-header = { 'User-agent' : 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1' }
-url='http://www.who.is/whois/'
-# =============================================
-class Browser:
-	def __init__(self):
-		"Creating Handler For Extracting Who.is site Webpage"
-		self.download_webpage()
 
-	def download_webpage(self):
-		domain=sys.argv[1]
-		request=urllib2.Request(url, headers=header)
-		request=urllib2.urlopen(request)
-		self.data=request.read()
-		return
-if __name__ == '__main__':
-	Browser()
+url="http://who.is/whois/"
+website=sys.argv[1]
+
+print "[*] Please Wait.... Connecting To Who.is Server.."
+htmldata=urllib2.urlopen(url+website).read()
+class_name="rawWhois"  # Class For Extraction
+
+try:
+    import lxml
+    parse=BeautifulSoup(htmldata,'lxml')
+    print "[*] Using lxml Module For Fast Extraction"
+except:
+    parse=BeautifulSoup(htmldata, "html.parser")
+    print "[*] Using built-in Html Parser [Slow Extraction. Please Wait ....]"
+
+try:
+    container=parse.findAll("div",{'class':class_name})
+    sections=container[1:]
+    for section in sections:
+        extract=section.findAll('div')
+        heading=extract[0].text
+        print '\n[ ',heading,' ]'
+        for i in extract[1].findAll('div'):
+            fortab='\t|'
+            for j in i.findAll('div'):
+                fortab=fortab+'----'
+                line=j.text.replace('\n', ' ')
+                print fortab,'>', line
+except Exception as e:
+    print "[ Error ] ", e
+    print "[ Last Update : 1 Jan 2017 ]"
+    print "[ Script Is Not Updated ]"
+    print "[ Sorry! ]"
+     
+    
